d381ba2785cac47d09009056a74ffa5b37d20fa8
hellozin <paul26375@gmail.com>
2021-02-12 02:28:06 +0900

    root 접근 시 endpoint path를 반환

--- src/main/java/me/hellozin/simple/echo/EchoController.java
+++ src/main/java/me/hellozin/simple/echo/EchoController.java
@@ -1,19 +1,32 @@
 package me.hellozin.simple.echo;
 
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @RestController
-public class EchoController {
+public class EchoController implements ApplicationListener<ContextRefreshedEvent> {
 
-    private static final String DEFAULT_MSG = "Hi, there.";
+    private Set<String> handlerMethodPatterns = new HashSet<>();
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        contextRefreshedEvent.getApplicationContext()
+                .getBean(RequestMappingHandlerMapping.class)
+                .getHandlerMethods()
+                .forEach((info, method) -> handlerMethodPatterns.add(info.getPatternValues().iterator().next()));
+    }
 
     @GetMapping("/")
-    public String root(@RequestParam(value="param", required = false) String param) {
+    public String root(@RequestParam(value = "param", required = false) String param) {
         return param != null
                 ? param
-                : DEFAULT_MSG;
+                : handlerMethodPatterns.toString();
     }
 
 }
